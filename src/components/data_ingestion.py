7b8a59411606b2822e5994e14aea8016f52535fa
shreyashpadwal <shreyash.22420202@viit.ac.in>
2025-09-14 13:36:50 +0530

    Data Ingestion and artifacts creation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -30,6 +30,7 @@ class DataIngestion:
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Ingestion of the data is completed")
 
